d5b212568d6ac3e9a1ab161ce49e5da74daef404
orazweb <jer.paoli@gmail.com>
2020-06-07 22:00:11 +0200

    Header and footer fragments done

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -1,11 +1,17 @@
 package com.example.thymeleaf.controller;
 
+import com.example.thymeleaf.model.Doctor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class DoctorController {
+
+    List<Doctor> doctors = new ArrayList<>();
 
     @GetMapping("/doctor/")
     public String doctor(@RequestParam(required=false, defaultValue="0") int number,
@@ -13,4 +19,5 @@ public class DoctorController {
 
         return "doctor";
     }
+
 }
--- src/main/java/com/example/thymeleaf/model/Doctor.java
+++ src/main/java/com/example/thymeleaf/model/Doctor.java
@@ -1,6 +1,6 @@
 package com.example.thymeleaf.model;
 
-class Doctor {
+public class Doctor {
 
     private int number;
     private String name;
